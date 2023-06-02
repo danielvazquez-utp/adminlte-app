02a0cde6bcb47f97653b2a2a8275d3b430141342
Daniel Vázquez <daniel.vazquez@utpuebla.edu.mx>
2023-06-02 09:30:04 -0600

    Componentes de Login y Datatable agregados

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,41 @@
-import * as React from 'react';
 import TopNavigation from './components/TopNavigation';
-
+import LoginBox from './components/LoginBox';
+import { useState } from 'react';
+import AddForm from './components/AddForm';
+import ListForm from './components/ListForm';
 
 const App = () => {
+
+  const [location, setLocation] = useState('listform');
+
+  const getLocation = () => {
+    if (location == 'login' ) {
+      return (
+        <LoginBox />
+      )
+    }
+    if (location == 'topnav') {
+      return (
+        <TopNavigation />
+      )
+    }
+    if (location == 'addform') {
+      return (
+        <AddForm />
+      )
+    }
+    if (location == 'listform') {
+      return (
+        <ListForm />
+      )
+    }
+  }
+
   return (
-    <TopNavigation />
+    <>
+      { getLocation() }
+    </>
   )
 }
 
-export default App
+export default App;
