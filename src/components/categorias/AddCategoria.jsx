95386e828fcb0850ea597306c829fa5bed574e5b
Daniel Vázquez <daniel.vazquez@utpuebla.edu.mx>
2023-06-30 09:57:11 -0600

    Fetch de registro de nueva categoria

--- src/components/categorias/AddCategoria.jsx
+++ src/components/categorias/AddCategoria.jsx
@@ -3,8 +3,33 @@ import { Link } from 'react-router-dom';
 import Aside from "../Aside";
 import Footer from "../Footer";
 import Navbar from "../Navbar";
+import { useState } from 'react';
 
 const AddCategoria = () => {
+
+    const [nombre, setNombre] = useState('');
+    const [estado, setEstado] = useState(1);
+
+    const handleAdd = () => {
+        console.log(nombre, estado);
+        var formdata = new FormData();
+        formdata.append("nombre", nombre);
+        formdata.append("activo", estado);
+        var requestOptions = {
+            mode: 'no-cors',
+	        headers: {
+                'Content-Type' : "application/json; charset=UTF-8",
+            },
+            method: 'POST',
+            body: formdata,
+          };
+          
+          fetch("http://localhost/codeigniter3-api-rest/index.php/Api/Categorias", requestOptions)
+            .then(response => response.text())
+            .then(result => console.log(result))
+            .catch(error => console.log('error', error));
+    }
+
   return (
     <>
         <Navbar />
@@ -43,20 +68,38 @@ const AddCategoria = () => {
                             <div className="card-body">
                                         <div className='form-group'>
                                             <label>Nombre de la categoría</label>
-                                            <input type='text' placeholder='Escribir aquí' name='nombre' id='nombre' className='form-control' />
+                                            <input 
+                                                type='text' 
+                                                placeholder='Escribir aquí'
+                                                name='nombre'
+                                                id='nombre'
+                                                className='form-control'
+                                                value={ nombre } 
+                                                onChange={ event => setNombre(event.target.value) }
+                                            />
                                         </div>
                                         <div className='form-group'>
                                             <label>Activo</label>
-                                            <select className='form-control' name='activo' id='activo'>
-                                                <option>-Seleccionar-</option>
+                                            <select 
+                                                className='form-control'
+                                                name='activo'
+                                                id='activo'
+                                                defaultValue={ estado }
+                                                onChange={ event => setEstado(event.target.value) }
+                                            >
                                                 <option value={1}>Si</option>
-                                                <option value={2}>No</option>
+                                                <option value={0}>No</option>
                                             </select>
                                         </div>
                             </div>
                             <div className="card-footer">
                                 <button className='btn btn-secondary'>Cancelar</button>
-                                <button className='btn btn-info btn-lg float-right'>Aceptar</button>
+                                <button 
+                                    className='btn btn-info btn-lg float-right'
+                                    onClick={ () => handleAdd() }
+                                >
+                                    Aceptar
+                                </button>
                             </div>
                         </div>
                     </div>
